f1d06e2f02d71ec512eedf99493ba1d05a340852
DELIRIUM-3D <micael_505@hotmail.com>
2025-06-02 23:04:41 +0200

    intro.js

--- js/intro.js
+++ js/intro.js
@@ -1,1 +1,8 @@
-// animación de entrada con salto por clic o scroll
+// animación de entrada con salto por clic o scrolldocument.addEventListener('DOMContentLoaded', () => {
+  const intro = document.getElementById('intro');
+  const skipIntro = () => intro.classList.add('hidden');
+
+  setTimeout(skipIntro, 3000); // Oculta tras 3 segundos
+  window.addEventListener('click', skipIntro);
+  window.addEventListener('scroll', skipIntro);
+});
